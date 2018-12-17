fesh0r/fernflower

commit 82a2939271c55732637cf921ba447af6caef796b

java-decompiler: cleanups and fixes
- duplicates eliminated
- immutable fields
- typos
- formatting

src/org/jetbrains/java/decompiler/struct/gen/generics/GenericType.java
@@ -16,6 +16,7 @@
 package org.jetbrains.java.decompiler.struct.gen.generics;
 
 import org.jetbrains.java.decompiler.code.CodeConstants;
+import org.jetbrains.java.decompiler.struct.gen.VarType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,66 +28,59 @@ public class GenericType {
   public static final int WILDCARD_UNBOUND = 3;
   public static final int WILDCARD_NO = 4;
 
-  public int type;
+  public final int type;
+  public final int arrayDim;
+  public final String value;
 
-  public int arraydim;
+  private final List<GenericType> enclosingClasses = new ArrayList<GenericType>();
+  private final List<GenericType> arguments = new ArrayList<GenericType>();
+  private final List<Integer> wildcards = new ArrayList<Integer>();
 
-  public String value;
-
-
-  private List<GenericType> enclosingClasses = new ArrayList<GenericType>();
-
-  private List<GenericType> arguments = new ArrayList<GenericType>();
-
-  private List<Integer> wildcards = new ArrayList<Integer>();
-
-
-  public GenericType(int type, int arraydim, String value) {
+  public GenericType(int type, int arrayDim, String value) {
     this.type = type;
-    this.arraydim = arraydim;
+    this.arrayDim = arrayDim;
     this.value = value;
   }
 
-
-  public GenericType(String strtype) {
-
-    parseSignature(strtype);
-  }
-
-  private void parseSignature(String sig) {
+  public GenericType(String signature) {
+    int type = 0;
+    int arrayDim = 0;
+    String value = null;
 
     int index = 0;
-    while (index < sig.length()) {
-
-      switch (sig.charAt(index)) {
+    loop:
+    while (index < signature.length()) {
+      switch (signature.charAt(index)) {
         case '[':
-          arraydim++;
+          arrayDim++;
           break;
+
         case 'T':
           type = CodeConstants.TYPE_GENVAR;
-          value = sig.substring(index + 1, sig.length() - 1);
-          return;
+          value = signature.substring(index + 1, signature.length() - 1);
+          break loop;
+
         case 'L':
           type = CodeConstants.TYPE_OBJECT;
-          sig = sig.substring(index + 1, sig.length() - 1);
+          signature = signature.substring(index + 1, signature.length() - 1);
 
           while (true) {
-            String cl = getNextClassSignature(sig);
+            String cl = getNextClassSignature(signature);
 
             String name = cl;
             String args = null;
 
-            int argfrom = cl.indexOf("<");
-            if (argfrom >= 0) {
-              name = cl.substring(0, argfrom);
-              args = cl.substring(argfrom + 1, cl.length() - 1);
+            int argStart = cl.indexOf("<");
+            if (argStart >= 0) {
+              name = cl.substring(0, argStart);
+              args = cl.substring(argStart + 1, cl.length() - 1);
             }
 
-            if (cl.length() < sig.length()) {
-              sig = sig.substring(cl.length() + 1); // skip '.'
-              GenericType type = new GenericType(CodeConstants.TYPE_OBJECT, 0, name);
-              parseArgumentsList(args, type);
-              enclosingClasses.add(type);
+            if (cl.length() < signature.length()) {
+              signature = signature.substring(cl.length() + 1); // skip '.'
+              GenericType type11 = new GenericType(CodeConstants.TYPE_OBJECT, 0, name);
+              parseArgumentsList(args, type11);
+              enclosingClasses.add(type11);
             }
             else {
               value = name;
@@ -95,18 +89,22 @@ public class GenericType {
             }
           }
 
-          return;
+          break loop;
+
         default:
-          value = sig.substring(index, index + 1);
-          type = getType(value.charAt(0));
+          value = signature.substring(index, index + 1);
+          type = VarType.getType(value.charAt(0));
       }
 
       index++;
     }
+
+    this.type = type;
+    this.arrayDim = arrayDim;
+    this.value = value;
   }
 
   private static String getNextClassSignature(String value) {
-
     int counter = 0;
     int index = 0;
 
@@ -132,18 +130,16 @@ public class GenericType {
   }
 
   private static void parseArgumentsList(String value, GenericType type) {
-
     if (value == null) {
       return;
     }
 
     while (value.length() > 0) {
-
-      String tstr = getNextType(value);
-      int len = tstr.length();
+      String typeStr = getNextType(value);
+      int len = typeStr.length();
       int wildcard = WILDCARD_NO;
 
-      switch (tstr.charAt(0)) {
+      switch (typeStr.charAt(0)) {
         case '*':
           wildcard = WILDCARD_UNBOUND;
           break;
@@ -158,41 +154,40 @@ public class GenericType {
       type.getWildcards().add(wildcard);
 
       if (wildcard != WILDCARD_NO) {
-        tstr = tstr.substring(1);
+        typeStr = typeStr.substring(1);
       }
 
-      type.getArguments().add(tstr.length() == 0 ? null : new GenericType(tstr));
+      type.getArguments().add(typeStr.length() == 0 ? null : new GenericType(typeStr));
 
       value = value.substring(len);
     }
   }
 
   public static String getNextType(String value) {
-
     int counter = 0;
     int index = 0;
 
-    boolean contmode = false;
+    boolean contMode = false;
 
     loop:
     while (index < value.length()) {
       switch (value.charAt(index)) {
         case '*':
-          if (!contmode) {
+          if (!contMode) {
             break loop;
           }
           break;
         case 'L':
         case 'T':
-          if (!contmode) {
-            contmode = true;
+          if (!contMode) {
+            contMode = true;
           }
         case '[':
         case '+':
         case '-':
           break;
         default:
-          if (!contmode) {
+          if (!contMode) {
             break loop;
           }
           break;
@@ -214,53 +209,18 @@ public class GenericType {
     return value.substring(0, index + 1);
   }
 
-  private static int getType(char c) {
-    switch (c) {
-      case 'B':
-        return CodeConstants.TYPE_BYTE;
-      case 'C':
-        return CodeConstants.TYPE_CHAR;
-      case 'D':
-        return CodeConstants.TYPE_DOUBLE;
-      case 'F':
-        return CodeConstants.TYPE_FLOAT;
-      case 'I':
-        return CodeConstants.TYPE_INT;
-      case 'J':
-        return CodeConstants.TYPE_LONG;
-      case 'S':
-        return CodeConstants.TYPE_SHORT;
-      case 'Z':
-        return CodeConstants.TYPE_BOOLEAN;
-      case 'V':
-        return CodeConstants.TYPE_VOID;
-      case 'G':
-        return CodeConstants.TYPE_GROUP2EMPTY;
-      case 'N':
-        return CodeConstants.TYPE_NOTINITIALIZED;
-      case 'A':
-        return CodeConstants.TYPE_ADDRESS;
-      case 'X':
-        return CodeConstants.TYPE_BYTECHAR;
-      case 'Y':
-        return CodeConstants.TYPE_SHORTCHAR;
-      case 'U':
-        return CodeConstants.TYPE_UNKNOWN;
-      default:
-        throw new RuntimeException("Invalid type");
-    }
+  public GenericType decreaseArrayDim() {
+    assert arrayDim > 0 : this;
+    return new GenericType(type, arrayDim - 1, value);
   }
-
 
   public List<GenericType> getArguments() {
     return arguments;
   }
 
-
   public List<GenericType> getEnclosingClasses() {
     return enclosingClasses;
   }
-
 
   public List<Integer> getWildcards() {
     return wildcards;

src/org/jetbrains/java/decompiler/code/interpreter/InstructionImpact.java
@@ -429,7 +429,7 @@ public class InstructionImpact {
         break;
       case CodeConstants.opc_aaload:
         var1 = stack.pop(2);
-        stack.push(new VarType(var1.type, var1.arraydim - 1, var1.value));
+        stack.push(new VarType(var1.type, var1.arrayDim - 1, var1.value));
         break;
       case CodeConstants.opc_astore:
         data.setVariable(instr.getOperand(0), stack.pop());
@@ -457,7 +457,7 @@ public class InstructionImpact {
         ck = pool.getLinkConstant(instr.getOperand(0));
         var1 = new VarType(ck.descriptor);
         stack.push(var1);
-        if (var1.stack_size == 2) {
+        if (var1.stackSize == 2) {
           stack.push(new VarType(CodeConstants.TYPE_GROUP2EMPTY));
         }
         break;
@@ -466,7 +466,7 @@ public class InstructionImpact {
       case CodeConstants.opc_putstatic:
         ck = pool.getLinkConstant(instr.getOperand(0));
         var1 = new VarType(ck.descriptor);
-        stack.pop(var1.stack_size);
+        stack.pop(var1.stackSize);
         break;
       case CodeConstants.opc_invokevirtual:
       case CodeConstants.opc_invokespecial:
@@ -478,11 +478,11 @@ public class InstructionImpact {
           ck = pool.getLinkConstant(instr.getOperand(0));
           MethodDescriptor md = MethodDescriptor.parseDescriptor(ck.descriptor);
           for (int i = 0; i < md.params.length; i++) {
-            stack.pop(md.params[i].stack_size);
+            stack.pop(md.params[i].stackSize);
           }
           if (md.ret.type != CodeConstants.TYPE_VOID) {
             stack.push(md.ret);
-            if (md.ret.stack_size == 2) {
+            if (md.ret.stackSize == 2) {
               stack.push(new VarType(CodeConstants.TYPE_GROUP2EMPTY));
             }
           }
@@ -494,9 +494,7 @@ public class InstructionImpact {
         break;
       case CodeConstants.opc_newarray:
         stack.pop();
-        var1 = new VarType(arr_type[instr.getOperand(0) - 4]);
-        var1.arraydim = 1;
-        stack.push(var1);
+        stack.push(new VarType(arr_type[instr.getOperand(0) - 4], 1).resizeArrayDim(1));
         break;
       case CodeConstants.opc_athrow:
         var1 = stack.pop();
@@ -516,7 +514,7 @@ public class InstructionImpact {
         cn = pool.getPrimitiveConstant(instr.getOperand(0));
         if (cn.isArray) {
           var1 = new VarType(CodeConstants.TYPE_OBJECT, 0, cn.getString());
-          var1.arraydim += dimensions;
+          var1 = var1.resizeArrayDim(var1.arrayDim + dimensions);
           stack.push(var1);
         }
         else {

src/org/jetbrains/java/decompiler/main/AssertProcessor.java
@@ -89,7 +89,7 @@ public class AssertProcessor {
             if (initializer.type == Exprent.EXPRENT_FUNCTION) {
               FunctionExprent fexpr = (FunctionExprent)initializer;
 
-              if (fexpr.getFunctype() == FunctionExprent.FUNCTION_BOOLNOT &&
+              if (fexpr.getFuncType() == FunctionExprent.FUNCTION_BOOL_NOT &&
                   fexpr.getLstOperands().get(0).type == Exprent.EXPRENT_INVOCATION) {
 
                 InvocationExprent invexpr = (InvocationExprent)fexpr.getLstOperands().get(0);
@@ -101,7 +101,7 @@ public class AssertProcessor {
                     invexpr.getLstParameters().isEmpty()) {
 
                   ConstExprent cexpr = (ConstExprent)invexpr.getInstance();
-                  if (VarType.VARTYPE_CLASS.equals(cexpr.getConsttype())) {
+                  if (VarType.VARTYPE_CLASS.equals(cexpr.getConstType())) {
 
                     ClassNode nd = node;
                     while (nd != null) {
@@ -173,7 +173,7 @@ public class AssertProcessor {
 
     Exprent ascond = null, retcond = null;
     if (exprres[0] != null) {
-      ascond = new FunctionExprent(FunctionExprent.FUNCTION_BOOLNOT,
+      ascond = new FunctionExprent(FunctionExprent.FUNCTION_BOOL_NOT,
                                    Arrays.asList((Exprent)exprres[0]), throwError.bytecode);
       retcond = SecondaryFunctionsHelper.propagateBoolNot(ascond);
     }
@@ -245,9 +245,9 @@ public class AssertProcessor {
 
     if (expr.type == Exprent.EXPRENT_EXIT) {
       ExitExprent exexpr = (ExitExprent)expr;
-      if (exexpr.getExittype() == ExitExprent.EXIT_THROW && exexpr.getValue().type == Exprent.EXPRENT_NEW) {
+      if (exexpr.getExitType() == ExitExprent.EXIT_THROW && exexpr.getValue().type == Exprent.EXPRENT_NEW) {
         NewExprent nexpr = (NewExprent)exexpr.getValue();
-        if (CLASS_ASSERTION_ERROR.equals(nexpr.getNewtype()) && nexpr.getConstructor() != null) {
+        if (CLASS_ASSERTION_ERROR.equals(nexpr.getNewType()) && nexpr.getConstructor() != null) {
           return nexpr.getConstructor();
         }
       }
@@ -260,7 +260,7 @@ public class AssertProcessor {
 
     if (exprent.type == Exprent.EXPRENT_FUNCTION) {
       FunctionExprent fexpr = (FunctionExprent)exprent;
-      if (fexpr.getFunctype() == FunctionExprent.FUNCTION_CADD) {
+      if (fexpr.getFuncType() == FunctionExprent.FUNCTION_CADD) {
 
         for (int i = 0; i < 2; i++) {
           Exprent param = fexpr.getLstOperands().get(i);
@@ -295,7 +295,7 @@ public class AssertProcessor {
 
     if (exprent.type == Exprent.EXPRENT_FUNCTION) {
       FunctionExprent fparam = (FunctionExprent)exprent;
-      if (fparam.getFunctype() == FunctionExprent.FUNCTION_BOOLNOT &&
+      if (fparam.getFuncType() == FunctionExprent.FUNCTION_BOOL_NOT &&
           fparam.getLstOperands().get(0).type == Exprent.EXPRENT_FIELD) {
         FieldExprent fdparam = (FieldExprent)fparam.getLstOperands().get(0);
         if (classname.equals(fdparam.getClassname())

src/org/jetbrains/java/decompiler/main/ClassReference14Processor.java
@@ -190,7 +190,7 @@ public class ClassReference14Processor {
           CatchStatement cst = (CatchStatement)root.getFirst();
           if (cst.getStats().size() == 2 && cst.getFirst().type == Statement.TYPE_BASICBLOCK &&
               cst.getStats().get(1).type == Statement.TYPE_BASICBLOCK &&
-              cst.getVars().get(0).getVartype().equals(new VarType(CodeConstants.TYPE_OBJECT, 0, "java/lang/ClassNotFoundException"))) {
+              cst.getVars().get(0).getVarType().equals(new VarType(CodeConstants.TYPE_OBJECT, 0, "java/lang/ClassNotFoundException"))) {
 
             BasicBlockStatement body = (BasicBlockStatement)cst.getFirst();
             BasicBlockStatement handler = (BasicBlockStatement)cst.getStats().get(1);
@@ -247,13 +247,13 @@ public class ClassReference14Processor {
 
     if (exprent.type == Exprent.EXPRENT_FUNCTION) {
       FunctionExprent fexpr = (FunctionExprent)exprent;
-      if (fexpr.getFunctype() == FunctionExprent.FUNCTION_IIF) {
+      if (fexpr.getFuncType() == FunctionExprent.FUNCTION_IIF) {
         if (fexpr.getLstOperands().get(0).type == Exprent.EXPRENT_FUNCTION) {
           FunctionExprent headexpr = (FunctionExprent)fexpr.getLstOperands().get(0);
-          if (headexpr.getFunctype() == FunctionExprent.FUNCTION_EQ) {
+          if (headexpr.getFuncType() == FunctionExprent.FUNCTION_EQ) {
             if (headexpr.getLstOperands().get(0).type == Exprent.EXPRENT_FIELD &&
                 headexpr.getLstOperands().get(1).type == Exprent.EXPRENT_CONST &&
-                ((ConstExprent)headexpr.getLstOperands().get(1)).getConsttype().equals(VarType.VARTYPE_NULL)) {
+                ((ConstExprent)headexpr.getLstOperands().get(1)).getConstType().equals(VarType.VARTYPE_NULL)) {
 
               FieldExprent field = (FieldExprent)headexpr.getLstOperands().get(0);
               ClassNode fieldnode = DecompilerContext.getClassProcessor().getMapRootClasses().get(field.getClassname());

src/org/jetbrains/java/decompiler/main/ClassWriter.java
@@ -136,7 +136,7 @@ public class ClassWriter {
               firstParameter = false;
             }
 
-            index += md_content.params[i].stack_size;
+            index += md_content.params[i].stackSize;
           }
 
           buffer.append(") ->");
@@ -440,7 +440,7 @@ public class ClassWriter {
     if (initializer != null) {
       if (isEnum && initializer.type == Exprent.EXPRENT_NEW) {
         NewExprent nexpr = (NewExprent)initializer;
-        nexpr.setEnumconst(true);
+        nexpr.setEnumConst(true);
         buffer.append(nexpr.toJava(indent, tracer));
       }
       else {
@@ -512,7 +512,7 @@ public class ClassWriter {
             firstParameter = false;
           }
 
-          index += md_content.params[i].stack_size;
+          index += md_content.params[i].stackSize;
         }
 
         buffer.append(") {").appendLineSeparator();
@@ -692,16 +692,16 @@ public class ClassWriter {
 
             appendParameterAnnotations(buffer, mt, paramCount);
 
-            if (methodWrapper.varproc.getVarFinal(new VarVersionPaar(index, 0)) == VarTypeProcessor.VAR_FINALEXPLICIT) {
+            if (methodWrapper.varproc.getVarFinal(new VarVersionPaar(index, 0)) == VarTypeProcessor.VAR_EXPLICIT_FINAL) {
               buffer.append("final ");
             }
 
             if (descriptor != null) {
               GenericType parameterType = descriptor.params.get(i);
 
-              boolean isVarArg = (i == lastVisibleParameterIndex && mt.hasModifier(CodeConstants.ACC_VARARGS) && parameterType.arraydim > 0);
+              boolean isVarArg = (i == lastVisibleParameterIndex && mt.hasModifier(CodeConstants.ACC_VARARGS) && parameterType.arrayDim > 0);
               if (isVarArg) {
-                parameterType.arraydim--;
+                parameterType = parameterType.decreaseArrayDim();
               }
 
               String typeName = GenericMain.getGenericCastTypeName(parameterType);
@@ -717,11 +717,11 @@ public class ClassWriter {
               }
             }
             else {
-              VarType parameterType = md.params[i].copy();
+              VarType parameterType = md.params[i];
 
-              boolean isVarArg = (i == lastVisibleParameterIndex && mt.hasModifier(CodeConstants.ACC_VARARGS) && parameterType.arraydim > 0);
+              boolean isVarArg = (i == lastVisibleParameterIndex && mt.hasModifier(CodeConstants.ACC_VARARGS) && parameterType.arrayDim > 0);
               if (isVarArg) {
-                parameterType.decArrayDim();
+                parameterType = parameterType.decreaseArrayDim();
               }
 
               String typeName = ExprProcessor.getCastTypeName(parameterType);
@@ -745,7 +745,7 @@ public class ClassWriter {
             paramCount++;
           }
 
-          index += md.params[i].stack_size;
+          index += md.params[i].stackSize;
         }
 
         buffer.append(')');
@@ -839,7 +839,7 @@ public class ClassWriter {
     return !hideMethod;
   }
 
-  private void mapLines(TextBuffer code, StructLineNumberTableAttribute table, BytecodeMappingTracer tracer, int startLine) {
+  private static void mapLines(TextBuffer code, StructLineNumberTableAttribute table, BytecodeMappingTracer tracer, int startLine) {
     // build line start offsets map
     HashMap<Integer, Set<Integer>> lineStartOffsets = new HashMap<Integer, Set<Integer>>();
     for (Map.Entry<Integer, Integer> entry : tracer.getMapping().entrySet()) {

src/org/jetbrains/java/decompiler/main/EnumProcessor.java
@@ -103,7 +103,7 @@ public class EnumProcessor {
         VarExprent instvar = (VarExprent)inv.getInstance();
         VarVersionPaar varpaar = new VarVersionPaar(instvar);
 
-        String classname = meth.varproc.getThisvars().get(varpaar);
+        String classname = meth.varproc.getThisVars().get(varpaar);
 
         if (classname != null) { // any this instance. TODO: Restrict to current class?
           if (!wrapper.getClassStruct().qualifiedName.equals(inv.getClassname())) {

src/org/jetbrains/java/decompiler/main/InitializerProcessor.java
@@ -261,7 +261,7 @@ public class InitializerProcessor {
       switch (expr.type) {
         case Exprent.EXPRENT_VAR:
           VarVersionPaar varpaar = new VarVersionPaar((VarExprent)expr);
-          if (!meth.varproc.getExternvars().contains(varpaar)) {
+          if (!meth.varproc.getExternalVars().contains(varpaar)) {
             String varname = meth.varproc.getVarName(varpaar);
 
             if (!varname.equals("this") && !varname.endsWith(".this")) { // FIXME: remove direct comparison with strings
@@ -308,7 +308,7 @@ public class InitializerProcessor {
         VarExprent instvar = (VarExprent)inv.getInstance();
         VarVersionPaar varpaar = new VarVersionPaar(instvar);
 
-        String classname = meth.varproc.getThisvars().get(varpaar);
+        String classname = meth.varproc.getThisVars().get(varpaar);
 
         if (classname != null) { // any this instance. TODO: Restrict to current class?
           if (withThis || !wrapper.getClassStruct().qualifiedName.equals(inv.getClassname())) {

src/org/jetbrains/java/decompiler/main/rels/ClassWrapper.java
@@ -124,7 +124,7 @@ public class ClassWrapper {
 
           int paramcount = 0;
           if (thisvar) {
-            varproc.getThisvars().put(new VarVersionPaar(0, 0), classStruct.qualifiedName);
+            varproc.getThisVars().put(new VarVersionPaar(0, 0), classStruct.qualifiedName);
             paramcount = 1;
           }
           paramcount += md.params.length;
@@ -138,11 +138,11 @@ public class ClassWrapper {
                 varindex++;
               }
               else {
-                varindex += md.params[i - 1].stack_size;
+                varindex += md.params[i - 1].stackSize;
               }
             }
             else {
-              varindex += md.params[i].stack_size;
+              varindex += md.params[i].stackSize;
             }
           }
         }

commit 4e0e3917981c97716b47fcaff3d0ee33a22e3c1e

[IDEA-204272] Fix varargs params losing generic info
Regression introduced in commit 82a2939271c55732637cf921ba447af6caef796b
"java-decompiler: cleanups and fixes"

src/org/jetbrains/java/decompiler/struct/gen/generics/GenericType.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.java.decompiler.struct.gen.generics;
 
 import org.jetbrains.java.decompiler.code.CodeConstants;
@@ -26,6 +26,17 @@ public class GenericType {
     this.type = type;
     this.arrayDim = arrayDim;
     this.value = value;
+  }
+
+  public GenericType(GenericType other, int arrayDim) {
+    this.type = other.type;
+    this.value = other.value;
+
+    this.enclosingClasses.addAll(other.enclosingClasses);
+    this.arguments.addAll(other.arguments);
+    this.wildcards.addAll(other.wildcards);
+
+    this.arrayDim = arrayDim;
   }
 
   public GenericType(String signature) {
@@ -197,7 +208,7 @@ public class GenericType {
 
   public GenericType decreaseArrayDim() {
     assert arrayDim > 0 : this;
-    return new GenericType(type, arrayDim - 1, value);
+    return new GenericType(this, arrayDim - 1);
   }
 
   public List<GenericType> getArguments() {